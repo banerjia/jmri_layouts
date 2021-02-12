7f6411fbbc5e0dca7fdb4ff34baf7351ef1eff46
Abhishek Banerji <webdesign.banerji@gmail.com>
2021-02-11 22:18:00 -0500

    Sensors and Panels
    Completed initial version of the code for creating panel lights based on sensor configuration and attaching them to the sensors.

--- CMRI_Scripting.jmri/scripts/create_sensors.py
+++ CMRI_Scripting.jmri/scripts/create_sensors.py
@@ -10,16 +10,14 @@ class CreateSenors(jmri.jmrit.automat.AbstractAutomaton) :
     def init(self):
         # init() is called exactly once at the beginning to do
         # any necessary configuration.
-        base_dir = "/home/banerjia/jmri_layouts/CMRI_Scripting.jmri/scripts/csv/test_layout"
+        base_dir = "/Users/banerjia/jmri_layouts/CMRI_Scripting.jmri/scripts/csv/test_layout"
         
 
-        TURNOUTS_NODE_ADDR = 1
+        PANEL_NODE_ADDR = 4
         SENSORS_NODE_ADDR = 1
-        MAX_TURNOUTS = 48
-        MAX_SENSORS = 24
 
         sensor_count  = 1
-        turnout_count  = 1
+        panel_light_count = 1
 
         # Create Blocks & Sensors
         file_path = "%s/sensors.csv" % (base_dir)
@@ -27,7 +25,7 @@ class CreateSenors(jmri.jmrit.automat.AbstractAutomaton) :
             csv_reader = csv.reader(csv_file, delimiter=',')
             line_count = 0
             for row in csv_reader:
-                if line_count > 1:
+                if line_count >= 1:
                     sensor_sys_name = "CS{:d}{:03d}".format(SENSORS_NODE_ADDR,sensor_count)                    
                     obj_sensor = jmri.InstanceManager.getDefault(jmri.SensorManager).newSensor(sensor_sys_name, row[0])   
                     obj_sensor.setComment(row[3])
@@ -36,6 +34,22 @@ class CreateSenors(jmri.jmrit.automat.AbstractAutomaton) :
                     for prop_key in props:
                         obj_sensor.setProperty(prop_key, props[prop_key])
                     sensors.register(obj_sensor)
+                    if row[1] == '1' and row[2] == '1':
+                        # Create a panel light entry for the sensor
+                        light_object_sys_name = "CL{}{:03d}".format(PANEL_NODE_ADDR,panel_light_count)
+                        light_object_user_name = "PNL IND {}".format(obj_sensor.getUserName())
+
+                        panelLight = lights.newLight(light_object_sys_name, light_object_user_name)
+                        panelLight.addLightControl(jmri.implementation.LightControl())
+                        lightControl = panelLight.getLightControlList()[-1]
+                        lightControl.setControlSensorName(obj_sensor.getUserName())
+                        lightControl.setControlType(jmri.Light.SENSOR_CONTROL)
+                        lightControl.setControlSensorSense(jmri.Sensor.ACTIVE)
+                        lightControl.setParentLight(panelLight)
+                        lightControl.activateLightControl()
+                        lights.register(panelLight)
+                        panel_light_count = panel_light_count + 1
+
                     sensor_count = sensor_count + 1    
                 line_count = line_count + 1
         return
