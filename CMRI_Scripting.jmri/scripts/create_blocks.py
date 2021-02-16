86374bb85fcce6b9828698283a6984be052c155d
Abs <webdesign.banerji@gmail.com>
2021-02-16 14:22:49 -0500

    Block, Turnouts and Sensors Scripts Refined
    The scripts for blocks, turnouts and sensors have been separated out. Based on the current setup the scripts for blocks and turnouts will need to be run first to create the objects. Then the sensor script will be run which will iterate through all the objects and create the necessary sensors. At this time no check is being done to make sure that existing objects are not being overwritten.

--- CMRI_Scripting.jmri/scripts/create_blocks.py
+++ CMRI_Scripting.jmri/scripts/create_blocks.py
@@ -28,7 +28,7 @@ class CreateBlocks(jmri.jmrit.automat.AbstractAutomaton) :
     """
      Script Configuration
     """
-    BASEDIR = "/Users/banerjia/jmri_layouts/CMRI_Scripting.jmri/resources"
+    BASEDIR = "/home/banerjia/jmri_layouts/CMRI_Scripting.jmri/resources"
             
     # Node Addresses
     NODE_ADDR = {
@@ -36,6 +36,9 @@ class CreateBlocks(jmri.jmrit.automat.AbstractAutomaton) :
         "TURNOUTS": 2,
         "PANELS" : 3
     }
+
+    # Station Config
+    STATION_CD = 'MML'
 
 
     def init(self):
@@ -53,7 +56,6 @@ class CreateBlocks(jmri.jmrit.automat.AbstractAutomaton) :
             panel_lights_count = 1
             turnouts_count = 1
             blocks_count = 1
-            blocks_prefix = 'TT1'
 
             for block_entry in fileBlocksReader:
                 # Skip Header Row
@@ -63,26 +65,11 @@ class CreateBlocks(jmri.jmrit.automat.AbstractAutomaton) :
                 
                 # Gather basic information
                 block_userName = block_entry[0]
+                block_userName_for_related_objects = block_userName.replace(self.STATION_CD, '').lstrip()
                 block_panel_ind = (block_entry[1] == '1')
                 block_comments = block_entry[2]
-                print(block_entry[3])
-                _prop_string = re.sub('(\w+):([^:]+)',r'"\1":"\2"',block_entry[3]).replace(';',',')
-                block_properties = '' #json.loads(_prop_string)
-
-                # Create Sensor
-                sensor_systemName = "CS{:d}{:03d}".format(self.NODE_ADDR["SENSORS"], sensors_count)
-                sensor_userName = "SNSR IND {}".format(block_userName)
-
-                sensors_count = sensors_count + 1
-
-                obj_sensor = sensors.provideSensor(sensor_systemName)
-                obj_sensor.setUserName(sensor_userName)
-                obj_sensor.setComment("Sensor for block occupancy of {}".format(block_userName))
-                obj_sensor.setProperty("Purpose", "BlockOccupancyDetection")
-                sensors.register(obj_sensor)
-
-                # Get NamedBeanHandle for the sensor to use with block
-                nmh_sensor = jmri.NamedBeanHandle(sensor_systemName, obj_sensor)
+                _prop_string = re.sub('(\w+):([^;}]+)',r'"\1":"\2"',block_entry[3]).replace(';',',')
+                block_properties = json.loads(_prop_string)
 
                 # Create Block and associate sensor
                 block_systemName = "IB{}:AUTO:{:03d}".format(self.NODE_ADDR["SENSORS"],blocks_count)
@@ -92,56 +79,14 @@ class CreateBlocks(jmri.jmrit.automat.AbstractAutomaton) :
                 obj_block = blocks.provideBlock(block_systemName)
                 obj_block.setUserName(block_userName)
                 obj_block.setComment(block_comments)
-                obj_block.setNamedSensor(nmh_sensor)
+                
                 for block_property in block_properties:
                     obj_block.setProperty(block_property, block_properties[block_property])
                 
+                obj_block.setProperty('PanelIndicator', block_panel_ind)
                 blocks.register(obj_block)
 
-                # Create light for panel if needed
-                if block_panel_ind:
-                    light_systemName = "CL{}{:03d}".format(self.NODE_ADDR["PANELS"],panel_lights_count)
-                    light_userName = "PNL IND {}".format(block_userName)
 
-                    panel_lights_count = panel_lights_count + 1
-
-                    obj_light = lights.provideLight(light_systemName)
-                    obj_light.setUserName(light_userName)
-                    obj_light.setComment("Block occupany indicator on panel for {}".format(block_userName))
-
-                    # Attach light to sensor
-                    obj_light.addLightControl(jmri.implementation.LightControl())
-                    obj_light_lightControl = obj_light.getLightControlList()[-1]
-                    obj_light_lightControl.setControlSensorName(sensor_userName)
-                    obj_light_lightControl.setControlType(jmri.Light.SENSOR_CONTROL)
-                    obj_light_lightControl.setControlSensorSense(jmri.Sensor.ACTIVE)
-                    obj_light_lightControl.setParentLight(obj_light)
-                    obj_light_lightControl.activateLightControl()
-
-                    lights.register(obj_light)
-                    """
-                    if block_properties["SensedObject"] != "BlockOverSwitch":
-                        light_systemName = "CL{}{:03d}".format(self.NODE_ADDR["PANELS"],panel_lights_count)
-                        light_userName = "PNL IND {}".format(block_userName)
-
-                        panel_lights_count = panel_lights_count + 1
-
-                        obj_light = lights.provideLight(light_systemName)
-                        obj_light.setUserName(light_userName)
-                        obj_light.setComment("Block occupany indicator on panel for {}".format(block_userName))
-
-                        # Attach light to sensor
-                        obj_light.addLightControl(jmri.implementation.LightControl())
-                        obj_light_lightControl = obj_light.getLightControlList()[-1]
-                        obj_light_lightControl.setControlSensorName(sensor_userName)
-                        obj_light_lightControl.setControlType(jmri.Light.SENSOR_CONTROL)
-                        obj_light_lightControl.setControlSensorSense(jmri.Sensor.ACTIVE)
-                        obj_light_lightControl.setParentLight(obj_light)
-                        obj_light_lightControl.activateLightControl()
-
-                        lights.register(obj_light)
-                    else:
-                    """
         return
 
     def handle(self):
